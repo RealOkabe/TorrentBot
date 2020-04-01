530b378b48e932d03af3bf2ec05cc62a8cd3fe33
MadScientistAK <anmolzagrawal@gmail.com>
2020-04-02 01:33:28 +0530

    Made a function that takes in a query and returns the list of torrents along with the links to them

---
+++ 1337.py
@@ -0,0 +1,53 @@
+import requests
+#import urllib.request
+#import time
+from bs4 import BeautifulSoup
+
+
+
+baseurl = 'https://1337x.to'
+
+# Function to search on 1337x.to
+def searchtor(quer):
+
+    # To check if the user's argument contains spaces and make url accordingly
+    if len(quer.split(' ')) > 1:
+        tempurl = baseurl + '/search/'
+        for i in quer.split(' '):
+            tempurl = tempurl + f'{i}+'
+        tempurl = tempurl[:len(tempurl)-1] + '/1/'
+    else:
+        tempurl = baseurl + f'/search/{quer}/1/'
+
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
+    headers =  {'User-Agent': user_agent}
+    res = requests.get(tempurl, headers = headers)
+
+    if str(res) == '<Response [200]>' and res.text != '':
+        # Finding all links
+        parsed = BeautifulSoup(res.text, 'html.parser').findAll('a')
+
+        # If the result is not found only 37 links are found
+        if len(list(parsed)) == 37:
+            return('No results found for the search. Please try again.')
+        else:
+            # Since first 35 a contain useless urls
+            torList = []
+            urlList = []
+            for i in parsed:
+                temp = str(i['href'])
+                # To find the torrents
+                if '/torrent/' in temp:
+                    # Splitting the a href and getting the value
+                    temp1 = str(i).split('>')[1].split('<')[0].replace('.', ' ')
+                    torList.append(temp1)
+                    urlList.append(baseurl + temp)
+
+
+        return(dict(zip(torList, urlList)))
+    else:
+        return("It seems there is a problem with the bot please contact @RintarouOkabe")
+
+a = input("Enter search query : ")
+
+print(searchtor(a))
