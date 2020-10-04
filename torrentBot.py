515eabce413ecebc7516e8269f6005e2184defc8
MadScientistAK <anmolzagrawal@gmail.com>
2020-10-04 14:58:20 +0530

    Made the bot inline using my own go api.

--- torrentBot.py
+++ torrentBot.py
@@ -1,87 +1,50 @@
-import json, os, telegram, logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Dispatcher
-from torscraper import searchOn1337, searchOnNyaa, getMagnetFrom1337
+import logging, json
+from torscraper2 import getTorrents, getAnimeTorrents
+from telegram.ext import Updater, InlineQueryHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+
 
 # Your bot token here.
 token = ''
-bot = telegram.Bot(token)
-id = ''
 updater = Updater(token, use_context=True)
 dispatcher = updater.dispatcher
-fileName1337 = 'tor.json'
-fileNameNyaa = 'animetor.json'
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-#logging to check for errors. These logs are not saved.
-# logging.basicConfig(level=logging.DEBUG,
-                    # format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-# Function that takes a query on telegram, searches for it on 1337x.to and returns the results to the user.
-def search1337(update, context):
-    if os.path.exists(fileNameNyaa):
-        os.remove(fileNameNyaa)
-    quer = update.message.text.split(None, 1)[1]
-    id = update.message.chat_id
-    results = searchOn1337(quer)
-    if isinstance(results, str):
-        bot.sendMessage(id, f"<b>{results}</b>", parse_mode="html")
+def torrentsSearch(update, context):
+    query = str(update.inline_query.query)
+    if not query or (not query.startswith('t') and not query.startswith('a')) or len(query.split()) < 2:
+        return
+    flag = query.split(None, 1)[0]
+    quer = query.split(None, 1)[1]
+    print(query)
+    print(quer)
+    if flag == 't':
+        tempres = json.loads(getTorrents(quer))
     else:
-        tors = 'Please select which torrent you would like to mirror: \n\n'
-        count = 1
-        for i in results.values():
-            tors = tors + str(count) + '. ' + f"<code>{i['name']}</code>" + "\n<b>Seeders:</b> " + f"<code>{i['seeders']}</code>" + "\n<b>Leechers:</b> " + f"<code>{i['leechers']}</code>" + "\n<b>Date Added:</b> " + f"<code>{i['date_added']}</code>" + "\n<b>Size:</b> " + f"<code>{i['size']}</code>" + '\n\n'
-            count = count + 1
-        bot.sendMessage(id, tors, parse_mode="html")
-        with open(fileName1337, 'w') as torFile:
-            json.dump(results, torFile)
-
-# Function that takes a query on telegram, searches for it on nyaa.si and returns the results to the user.
-def searchNyaa(update, context):
-    if os.path.exists(fileName1337):
-        os.remove(fileName1337)
-    quer = update.message.text.split(None, 1)[1]
-    id = update.message.chat_id
-    results = searchOnNyaa(quer)
-    if isinstance(results, str):
-        bot.sendMessage(id, f"<b>{results}</b>", parse_mode="html")
+        tempres = json.loads(getAnimeTorrents(quer))
+    if len(tempres) > 30:
+        tempres = tempres[:30]
+    results = list()
+    if not tempres:
+        results.append(
+            InlineQueryResultArticle(
+                id=1,
+                title="No results found. Please refine search.",
+                input_message_content=InputTextMessageContent(tempres)
+            )
+        )
     else:
-        tors = 'Please select the anime torrent whose magnet link you want: \n\n'
-        count = 1
-        for i in results.values():
-            tors = tors + str(count) + '. ' + f"<code>{i['name']}</code>" + "\n<b>Seeders:</b> " + f"<code>{i['seeders']}</code>" + "\n<b>Leechers:</b> " + f"<code>{i['leechers']}</code>" + "\n<b>Date Added:</b> " + f"<code>{i['date_added']}</code>" + "\n<b>Size:</b> " + f"<code>{i['size']}</code>" + '\n\n'
-            count = count + 1
-        bot.sendMessage(id, tors, parse_mode="html")
-        with open(fileNameNyaa, 'w') as torFile:
-            json.dump(results, torFile)
+        temp = 1
+        for i in tempres:
+            results.append(InlineQueryResultArticle(
+                id=f'{quer.upper()}{temp}',
+                title=i['Name'],
+                input_message_content=InputTextMessageContent(i['Magnetlink'])
+            ))
+            temp = temp + 1
+        print(results)
+    context.bot.answer_inline_query(update.inline_query.id, results)
 
-
-# Function that takes a number message on telegram as user input and returns the magnet link of the torrent requested.
-def sendMagnet(update, context):
-    userChoice = update.message.text
-    id = update.message.chat_id
-    if os.path.exists(fileName1337):
-        if userChoice.isdigit():
-            torChoice = int(userChoice)
-            if len(userChoice) <=2 and len(userChoice) > 0 and torChoice <= 10:
-                with open(fileName1337) as torfile:
-                    torrdata = list(json.load(torfile).values())
-                msg = getMagnetFrom1337(torrdata[torChoice - 1]['url'])
-                bot.sendMessage(id, msg)
-                os.remove(fileName1337)
-    elif os.path.exists(fileNameNyaa):
-        if userChoice.isdigit():
-            torChoice = int(userChoice)
-            if len(userChoice) <=2 and len(userChoice) > 0 and torChoice <= 10:
-                with open(fileNameNyaa) as torfile:
-                    torrdata = list(json.load(torfile).values())
-                msg = torrdata[torChoice - 1]['url']
-                bot.sendMessage(id, msg)
-                os.remove(fileNameNyaa)
-
-if __name__ == '__main__':    
-    handler1337 = CommandHandler('torrent', search1337)
-    handlerNyaa = CommandHandler('animetorrent', searchNyaa)
-    magnethandler = MessageHandler(Filters.text, sendMagnet)
-    dispatcher.add_handler(handler1337)
-    dispatcher.add_handler(handlerNyaa)
-    dispatcher.add_handler(magnethandler)
-    updater.start_polling()
+search_handler = InlineQueryHandler(torrentsSearch)
+dispatcher.add_handler(search_handler)
+updater.start_polling()
--- torscraper.py
+++ torscraper.py
@@ -1,84 +1,14 @@
 import requests
-from bs4 import BeautifulSoup
 
-url_nyaa = 'https://nyaa.si'
-url_1337 = 'https://1337x.to'
-user_agent = 'api'
-headers =  {'User-Agent': user_agent}
+urltor = "https://torscraper.herokuapp.com/search1337/"
+urlanime = "https://torscraper.herokuapp.com/searchNyaa/"
 
-# What contents does is that it converts the html inside a tag to a list of tags inside it
+def getTorrents(query):
+    rawdata = requests.get(urltor + query)
+    return rawdata.text
 
-def searchOnNyaa(quer):
-    search_url = f'{url_nyaa}/?f=0&c=1_2&q={quer.replace(" ", "+")}'
-    r = requests.get(search_url, headers = headers)
-    parsed = BeautifulSoup(r.text, "html.parser")
+def getAnimeTorrents(query):
+    rawdata = requests.get(urlanime + query)
+    return rawdata.text
 
-    # Don't know why but nyaa has these three classes for their torrents in trs
-    usable_content = parsed.findAll('tr', {"class": {"default", "danger", "success"}})
-
-    number_of_torrents = len(usable_content)
-
-    if not number_of_torrents:
-        return "No Results Found"
-    # Limiting the number of torrents to 10 for convenience.
-    if number_of_torrents > 10:
-        usable_content = usable_content[:10]
-
-    torrentList = {}
-    count = 0
-
-    for i in usable_content:
-        temp_data = i.contents
-        torrent_name = temp_data[3].contents
-        torrent_name = torrent_name[3] if 'comment' in torrent_name[1]['title'] else torrent_name[1]
-        torrent_name = torrent_name['title']
-        torrent_link = temp_data[5].contents[3]['href']
-        torrent_size = temp_data[7].text
-        torrent_date_added = temp_data[9].text
-        torrent_seeders = temp_data[11].text
-        torrent_leechers = temp_data[13].text
-        torrentList[f't{count}'] = {'name': torrent_name, 'url': torrent_link, 'seeders': torrent_seeders, 'leechers': torrent_leechers, 'date_added': torrent_date_added, 'size': torrent_size}
-        count = count + 1
-
-    return torrentList
-
-def searchOn1337(quer):
-    search_url = f"{url_1337}/search/{quer.replace(' ', '+')}/1/"
-    r = requests.get(search_url, headers = headers)
-    parsed = BeautifulSoup(r.text, "html.parser")
-
-    # It turns out 1337x has all torrents in trs and the first tr has categories and such.
-    usable_content = parsed.findAll('tr')
-    
-    number_of_torrents = len(usable_content)
-
-    if not number_of_torrents:
-        return "No Results Found"
-    
-    # Removed the tr that has categories and limited the number of torrents for convenience.
-    if number_of_torrents > 10:
-        usable_content = usable_content[1:11]
-
-    torrentList = {}
-    count = 0
-    
-    for i in usable_content:
-        temp_data = i.contents
-        torrent_name = temp_data[1].contents[1].text
-        torrent_link = url_1337 + temp_data[1].contents[1]['href']
-        torrent_seeders = temp_data[3].text
-        torrent_leechers = temp_data[5].text
-        torrent_date_added = temp_data[7].text
-        torrent_size = temp_data[9].contents[0]
-
-        torrentList[f't{count}'] = {'name': torrent_name, 'url': torrent_link, 'seeders': torrent_seeders, 'leechers': torrent_leechers, 'date_added': torrent_date_added, 'size': torrent_size}
-        count = count + 1
-
-    return torrentList
-
-def getMagnetFrom1337(torUrl):
-    r = requests.get(torUrl, headers = headers)
-    usable_data = BeautifulSoup(r.text, 'html.parser').findAll('div', {'class': 'no-top-radius'})
-    # Sorry for the complexity but this is the fastest way to parse to the magnet url of the torrent
-    usable_data = usable_data[0].contents[1].contents[1].contents[1].contents[0]
-    return usable_data['href']
+print(getTorrents('Mahouka'))
